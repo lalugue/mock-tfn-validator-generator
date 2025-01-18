a2eb6a45bb77a8ed329c63586f149f9b93d72d15
lalugue <51893731+lalugue@users.noreply.github.com>
2025-01-19 00:02:10 +0800

    add some spacing to links

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -2,9 +2,13 @@ import Link from "next/link";
 
 export default function NavBar() {
   return (
-    <div className="navbar">
-      <Link href="/">Home</Link>
-      <Link href="/generator">Generate TFN</Link>
+    <div className="navbar text-right m-4">
+      <Link className="mx-4" href="/">
+        Home
+      </Link>
+      <Link className="mx-4" href="/generator">
+        Generate TFN
+      </Link>
     </div>
   );
 }
