4205d75abff46a56d9011d1728c4b7c8b6a873fe
lalugue <51893731+lalugue@users.noreply.github.com>
2025-01-22 05:51:37 +0800

    add base prop for handling value change in tfninput

--- src/app/components/tfn_input.tsx
+++ src/app/components/tfn_input.tsx
@@ -1,10 +1,13 @@
-export default function TFNInput() {
+"use client";
+
+export default function TFNInput({ handleOnChange = () => {} }) {
   return (
     <input
       className="border-2 border-gray-400 focus:border-blue-300"
       placeholder="Enter TFN"
       type="text"
       name="tfn"
+      onChange={handleOnChange}
     />
   );
 }
