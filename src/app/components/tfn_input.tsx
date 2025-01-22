2c1a2610ffc22d6f5572a809fbca1ae546cbdf05
lalugue <51893731+lalugue@users.noreply.github.com>
2025-01-22 15:08:59 +0800

    define handleonchange prop more explicitly

--- src/app/components/tfn_input.tsx
+++ src/app/components/tfn_input.tsx
@@ -1,13 +1,18 @@
 "use client";
 
-export default function TFNInput({ handleOnChange = () => {} }) {
+interface TFNInputProps {
+  handleOnChange?: (value: string) => void;
+}
+export default function TFNInput({ handleOnChange = () => {} }: TFNInputProps) {
   return (
     <input
       className="border-2 border-gray-400 focus:border-blue-300"
       placeholder="Enter TFN"
       type="text"
       name="tfn"
-      onChange={handleOnChange}
+      onChange={(e) => {
+        handleOnChange(e.target.value);
+      }}
     />
   );
 }
