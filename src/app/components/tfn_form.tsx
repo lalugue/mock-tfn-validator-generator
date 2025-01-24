b63fc364a6a4dbce751a9594aef60b079e49dc61
lalugue <51893731+lalugue@users.noreply.github.com>
2025-01-24 15:23:55 +0800

    add base dedicated component for form

---
+++ src/app/components/tfn_form.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useContext } from "react";
+import { TFNContext } from "../providers/tfn_provider";
+import TFNInput from "./tfn_input";
+
+export default function TFNForm() {
+  // @TODO: add tfn as param for validation calls, etc
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { tfn, setTfn } = useContext(TFNContext);
+  return (
+    <div className="tfn-form">
+      <TFNInput handleOnChange={setTfn} />
+      <button
+        className="p-2 rounded-lg bg-green-500 hover:bg-green-400"
+        onClick={() => {
+          // @TODO: add validation calls, etc here
+          // noop
+        }}>
+        Validate
+      </button>
+    </div>
+  );
+}
