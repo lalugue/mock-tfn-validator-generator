561e6e7677fbab9993c4428882810d7d05d9cb79
lalugue <51893731+lalugue@users.noreply.github.com>
2025-04-02 00:18:13 +0800

    [tweak] transfer page layout bits to dedicated layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,10 +26,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <NavBar/>
-        {children}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <NavBar />
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+          <main className="flex flex-col gap-8 row-start-2 items-center">
+            {children}
+          </main>
+          <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
+        </div>
       </body>
     </html>
   );
