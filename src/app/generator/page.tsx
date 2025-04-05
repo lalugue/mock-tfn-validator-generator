0116edc8ca9291e9b6dab205d61ef13796a15128
lalugue <51893731+lalugue@users.noreply.github.com>
2025-04-05 08:25:59 +0800

    add dedicated state and span for generated tfn

--- src/app/generator/page.tsx
+++ src/app/generator/page.tsx
@@ -1,13 +1,16 @@
 "use client"
 
+import { useState } from "react";
 import { generateTFN } from "./utils";
 
 export default function TFNGenerator() {
+  const [tfn, setTfn] = useState<string | number>("");
   return (
     <div className="tfn-generator flex flex-col gap-2">
+      <span className="bg-gray-300 border-4 border-black border-dashed p-4">{tfn}</span>
       <button
         className="p-2 rounded-lg bg-green-500 hover:bg-green-400"
-        onClick={generateTFN}>
+        onClick={()=>{setTfn(generateTFN())}}>
         Generate
       </button>
     </div>
