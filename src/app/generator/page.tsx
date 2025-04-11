caa34a53d354991116a6bec0d43b07acbea3e85d
lalugue <51893731+lalugue@users.noreply.github.com>
2025-04-12 01:30:30 +0800

    tweak generator layout

--- src/app/generator/page.tsx
+++ src/app/generator/page.tsx
@@ -7,7 +7,7 @@ export default function TFNGenerator() {
   const [tfn, setTfn] = useState<string | number>("");
   return (
     <div className="tfn-generator flex flex-col gap-2">
-      <span className="bg-gray-300 border-4 border-black border-dashed p-4">
+      <span className="bg-gray-300 border-4 border-black border-dashed p-4 w-48 text-center">
         {tfn}
       </span>
       <button
