69ee5ba956ead71accada85f537840bf14ed3404
lalugue <51893731+lalugue@users.noreply.github.com>
2025-04-13 00:54:42 +0800

    purge home link at navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -3,9 +3,6 @@ import Link from "next/link";
 export default function NavBar() {
   return (
     <div className="navbar text-right m-4">
-      <Link className="mx-4" href="/">
-        Home
-      </Link>
       <Link className="mx-4" href="/validator">
         Validate TFN
       </Link>
