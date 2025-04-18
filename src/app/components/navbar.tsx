ecf13b370cf8b1b446b39153347371cd941b1428
lalugue <51893731+lalugue@users.noreply.github.com>
2025-04-18 10:04:57 +0800

    [tweak] rearrange navbar links

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 export default function NavBar() {
   return (
     <div className="navbar text-right m-4">
-      <Link className="mx-4" href="/validator">
-        Validate TFN
-      </Link>
       <Link className="mx-4" href="/generator">
         Generate TFN
+      </Link>
+      <Link className="mx-4" href="/validator">
+        Validate TFN
       </Link>
     </div>
   );
