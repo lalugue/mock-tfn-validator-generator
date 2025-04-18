794165925ba231b73e72068cadca3356a1b35a31
lalugue <51893731+lalugue@users.noreply.github.com>
2025-04-19 07:33:26 +0800

    redirect explicitly if deployed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
+  if (window.location.hostname !== "localhost") {
+    redirect("/generator");
+  }
+
   return (
     <div className="home">
       <p>Welcome to TFN Validator Home!</p>
