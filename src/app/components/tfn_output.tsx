f5cbc6a0929e69b23f354fee673160aa43941624
lalugue <51893731+lalugue@users.noreply.github.com>
2025-04-20 01:06:00 +0800

    tweak output colors vs dark mode

--- src/app/components/tfn_output.tsx
+++ src/app/components/tfn_output.tsx
@@ -11,7 +11,7 @@ const defaultTFNOutputProps = {
 export default function TFNOutput(props: TFNOutputProps) {
   const { tfn } = { ...defaultTFNOutputProps, ...props };
   return (
-    <span className="bg-gray-300 border-4 border-black border-dashed p-4 w-48 text-center">
+    <span className="bg-gray-300 border-4 border-black border-dashed p-4 w-48 text-center dark:bg-gray-300 dark:text-black">
       {tfn}
     </span>
   );
